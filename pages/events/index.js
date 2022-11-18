f3cc74c0ed90d95fc2ea500064f0697c26f7225a
Olena <pampam32121@gmail.com>
2022-11-18 16:41:37 +0200

    added dummy-data file, deleted previous dummy data from AllEventsPage

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,26 +1,8 @@
-import Link from 'next/link'
-
 export default function AllEventsPage() {
-
-    const data = [
-        {id: '1', name: 'Easter', year: 2021, featured: false},
-        {id: '2', name: 'Christmas', year: 2023, featured: false}
-    ]
 
     return (
         <div>
             <h1>All Events</h1>
-            <ul>
-                {data.map(event => (
-                    <li key={event.id}>
-                        <Link href={`/events/${event.id}`}>
-                            <h3>{event.name}</h3>
-                        </Link>
-                        <p>Year: {event.year}</p>
-                        <button>{event.featured ? 'Remove From Favorites' : 'To Favorites'}</button>
-                    </li>
-                ))}
-            </ul>
         </div>
     )
 }
