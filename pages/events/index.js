8addb21dd086a5921931115c9d8fd1e2877c9b61
Olena <pampam32121@gmail.com>
2022-11-19 21:37:27 +0200

    added EventsSearch component to the AllEventsPage, for now no logic included

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,16 @@
-import { getAllEvents } from "../../dummy-data"
-import EventList from "../../components/events/event-list";
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+import { Fragment } from 'react';
+
 export default function AllEventsPage() {
 
 const allEvents = getAllEvents();
 
     return (
-        <div>
+        <Fragment>
+            <EventsSearch />
             <EventList items={allEvents}/>
-        </div>
+        </Fragment>
     )
 }
