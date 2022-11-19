f342bddfce6d0aef4b7544f488e1c9626f1228d4
Olena <pampam32121@gmail.com>
2022-11-20 00:25:45 +0200

    polished project with adding error alerts and results components

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,10 @@
 import { useRouter } from 'next/router'
 import EventList from '../../components/events/event-list'
+import ResultsTitle from '../../components/events/results-title'
+import Button from '../../components/ui/button'
+import ErrorAlert from '../../components/ui/error-alert'
 import { getFilteredEvents } from '../../dummy-data'
+import { Fragment } from 'react'
 
 export default function FilteredEventsPage() {
     const router = useRouter()
@@ -21,7 +25,14 @@ export default function FilteredEventsPage() {
         numMonth > 12 ||
         numMonth < 1
     ) {
-        return <p className='center'>Invalid filter. Please adjuct your values!</p>
+        return (
+            <Fragment>
+                <ErrorAlert><p>Invalid filter. Please adjuct your values!</p></ErrorAlert>
+                <div className='center'>
+                    <Button link='/events'>Show All Events</Button>
+                </div>
+            </Fragment>
+        )
     }
 
     const filteredEvents = getFilteredEvents({
@@ -30,12 +41,22 @@ export default function FilteredEventsPage() {
     })
 
     if (!filteredEvents || !filteredEvents.length) {
-        return <p className='center'>No events found for the chosen date...</p>
+        return (
+            <Fragment>
+                <ErrorAlert><p>No events found for the chosen date...</p></ErrorAlert>
+                <div className='center'>
+                    <Button link='/events'>Show All Events</Button>
+                </div>
+            </Fragment>
+        )
     }
 
+    const date = new Date(numYear, numMonth - 1)
+
     return (
-        <div>
+        <Fragment>
+            <ResultsTitle date={date}/>
             <EventList items={filteredEvents}/>
-        </div>
+        </Fragment>
     )
 }
