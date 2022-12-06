e95ac665bf0ed6622882f1fefcf09d978f88eb15
Olena <pampam32121@gmail.com>
2022-12-06 03:20:41 +0200

    added a newsletter route

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,9 +1,23 @@
+import { useRef } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+
+  const emailInputRef = useRef()
+
   function registrationHandler(event) {
     event.preventDefault();
+    const enteredEmail = emailInputRef.current.value
 
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
     // fetch user input (state or refs)
     // optional: validate input
     // send valid data to API
@@ -15,10 +29,12 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
+            ref={emailInputRef}
             type='email'
             id='email'
             placeholder='Your email'
             aria-label='Your email'
+            required
           />
           <button>Register</button>
         </div>
