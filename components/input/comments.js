ea433d388ccb888ca4cbe7fc3140f88bd5ffde9f
Olena <pampam32121@gmail.com>
2022-12-06 04:23:42 +0200

    connected the frontend to the comments api routes

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
@@ -8,15 +8,33 @@ function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([])
+
+  useEffect( () => {
+    if (showComments) {
+      fetch(`/api/comments/${eventId}`)
+        .then(res => res.json())
+        .then(data => {
+          setComments(data.comments)
+        })
+    }
+  }, [showComments])
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
   function addCommentHandler(commentData) {
-    const { email, name, text } = commentData
-    console.log(email, name, text)
-    // send data to API
+    
+    fetch(`/api/comments/${eventId}`, {
+      method: 'POST',
+      body: JSON.stringify(commentData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
   }
 
   return (
@@ -25,7 +43,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList items={comments}/>}
     </section>
   );
 }
