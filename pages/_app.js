17882f6e28f0195e0c36a0034219ecc923035334
Olena <pampam32121@gmail.com>
2022-12-16 00:15:47 +0200

    added notification component to inform user about success/failure

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/layout'
 import Head from 'next/head'
+import Notification from '../components/ui/notification'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title='Test' status='success' message='success'/>
     </Layout>
   )
 }
