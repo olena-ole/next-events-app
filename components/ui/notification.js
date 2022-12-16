8b923472fbc0588e877b2d7c66630893966bab15
Olena <pampam32121@gmail.com>
2022-12-16 04:38:15 +0200

    added removing notification after clicking on it or after 3 seconds

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,6 +1,10 @@
-import classes from './notification.module.css';
+import { useContext } from 'react'
+
+import classes from './notification.module.css'
+import NotificationContext from '../../store/notification-context';
 
 function Notification(props) {
+  const notificationCtx = useContext(NotificationContext)
 
   const { title, message, status } = props;
 
@@ -21,7 +25,7 @@ function Notification(props) {
   const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    <div className={activeClasses} >
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
