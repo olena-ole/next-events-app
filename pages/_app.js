395c6a8dca9f26fb8cd4b4c75ea07ab4e001a654
Olena <pampam32121@gmail.com>
2022-12-16 03:08:16 +0200

    use context data in layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/layout'
 import Head from 'next/head'
-import Notification from '../components/ui/notification'
 import { NotificationContextProvider } from '../store/notification-context'
 
 function MyApp({ Component, pageProps }) {
@@ -14,7 +13,6 @@ function MyApp({ Component, pageProps }) {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <Component {...pageProps} />
-        <Notification title='Test' status='success' message='success'/>
       </Layout>
     </NotificationContextProvider>
   )
